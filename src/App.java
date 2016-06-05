4af40bbcf405fb43c3f2172d03f3215a1fb69fcb
grinkoan <konstantinos.anastasiou@myomegasys.com>
2016-06-05 21:29:40 +0200

    commit from command line

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
+import java.util.Date;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Date date = new Date(System.currentTimeMillis());
+		System.out.println(date.toString());
 
 	}
 
